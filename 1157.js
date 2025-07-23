a7491ffec929ac5ac4f22bac9bd86b553bc533b9
yongmin <oio337a@gmail.com>
2025-07-24 00:24:37 +0900

    1157 단어 공부

--- 1157.js
+++ 1157.js
@@ -1,26 +1,13 @@
 let fs = require('fs');
-let input = fs.readFileSync('test.txt').toString().split(' ');
+const str = fs.readFileSync('/dev/stdin').toString().replace(/\s+/g, '').toUpperCase();
+const uniqueStr = [...new Set(str)];
 
-const str = input[0].toUpperCase()
-const cntStr = new Set(str)
+let cntList = [];
+uniqueStr.forEach((char) => {
+    cntList.push(str.split('').filter(c => c === char).length);
+});
 
-let cntList = []
-cntStr.forEach((target) => {
-    let cnt = 0
-    str.split('').forEach((s) => {
-        if (target === s) cnt++
-    })
-    cntList.push(cnt)
-})
+const maxNum = Math.max(...cntList);
+const maxCnt = cntList.filter(c => c === maxNum).length;
 
-let maxNum = Math.max(...cntList)
-let maxCnt = 0
-cntList.forEach(e => {
-    if (e === maxNum) maxCnt++
-})
-
-if (maxCnt > 1) {
-    console.log('?')
-} else {
-    console.log([...cntStr][cntList.indexOf(maxNum)])
-}
+console.log(maxCnt > 1 ? '?' : uniqueStr[cntList.indexOf(maxNum)]);
