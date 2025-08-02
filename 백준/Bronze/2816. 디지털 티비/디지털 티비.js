1c792b0597e9ab6c1963393ef72c5bdbbf8a0a95
Yongmin Park <75519420+oio337a@users.noreply.github.com>
2025-08-02 19:55:53 +0900

    [Bronze I] Title: 디지털 티비, Time: 88 ms, Memory: 9392 KB -BaekjoonHub

---
+++ 백준/Bronze/2816. 디지털 티비/디지털 티비.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const n = parseInt(input[0], 10);
+const channels = input.slice(1);
+
+let answer = [];
+
+let cursor = 0;
+
+// KBS1을 맨 앞으로
+let kbs1 = channels.indexOf('KBS1');
+while (cursor < kbs1) {
+    answer.push('1');
+    cursor++;
+}
+while (cursor > 0) {
+    // swap current and above
+    [channels[cursor], channels[cursor - 1]] = [channels[cursor - 1], channels[cursor]];
+    answer.push('4');
+    cursor--;
+}
+
+// KBS2를 두 번째로
+let kbs2 = channels.indexOf('KBS2');
+while (cursor < kbs2) {
+    answer.push('1');
+    cursor++;
+}
+while (cursor > 1) {
+    [channels[cursor], channels[cursor - 1]] = [channels[cursor - 1], channels[cursor]];
+    answer.push('4');
+    cursor--;
+}
+
+console.log(answer.join(''));
