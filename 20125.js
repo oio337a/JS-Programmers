0c41c8d214356ba3a8c47dd71dc0b6be2680a812
yongmin <oio337a@gmail.com>
2025-08-03 19:11:53 +0900

    알고리즘 문제 clear

---
+++ 20125.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const input = fs.readFileSync('test.txt').toString().trim().split('\r\n');
+const N = parseInt(input[0]);
+const map = input.slice(1).map(line => line.split(''));
+
+// 1. 심장 찾기
+let heartRow = -1, heartCol = -1;
+
+for (let i = 0; i < N; i++) {
+    const idx = map[i].indexOf('*');
+    if (idx !== -1) {
+        // 심장은 첫 별 바로 아래 행의 가운데
+        heartRow = i + 1;
+        heartCol = idx;
+        break;
+    }
+}
+
+// 심장 위치는 1-based
+console.log(`${heartRow + 1} ${heartCol + 1}`);
+
+// 2. 왼팔
+let leftArm = 0;
+for (let c = heartCol - 1; c >= 0; c--) {
+    if (map[heartRow][c] === '*') leftArm++;
+    else break;
+}
+
+// 3. 오른팔
+let rightArm = 0;
+for (let c = heartCol + 1; c < N; c++) {
+    if (map[heartRow][c] === '*') rightArm++;
+    else break;
+}
+
+// 4. 허리
+let waist = 0;
+let waistEnd = heartRow + 1;
+for (let r = heartRow + 1; r < N; r++) {
+    if (map[r][heartCol] === '*') {
+        waist++;
+        waistEnd = r;
+    } else break;
+}
+
+// 5. 왼다리
+let leftLeg = 0;
+for (let r = waistEnd + 1; r < N; r++) {
+    if (map[r][heartCol - 1] === '*') leftLeg++;
+    else break;
+}
+
+// 6. 오른다리
+let rightLeg = 0;
+for (let r = waistEnd + 1; r < N; r++) {
+    if (map[r][heartCol + 1] === '*') rightLeg++;
+    else break;
+}
+
+console.log(`${leftArm} ${rightArm} ${waist} ${leftLeg} ${rightLeg}`);
---
+++ 7568.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const input = fs.readFileSync('test.txt').toString().split('\n')
+
+const [N, arr] = [input[0], input.slice(1).map(e => e.split(' ').map(Number))]
+let answer = []
+for (let i = 0; i < N; i++) {
+    let level = 1
+    for (let j = 0; j < N; j++) {
+        if (i === j) continue
+
+        if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) level++
+    }
+    
+    answer.push(level)
+}
+
+console.log(answer.join(' '))
