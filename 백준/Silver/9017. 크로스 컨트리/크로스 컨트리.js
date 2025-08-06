169f4a3126c7f5a3207283f8be1c0955ae94e277
Yongmin Park <75519420+oio337a@users.noreply.github.com>
2025-08-07 00:08:01 +0900

    [Silver III] Title: 크로스 컨트리, Time: 176 ms, Memory: 11864 KB -BaekjoonHub

---
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const T = parseInt(input[0]);
+let idx = 1;
+
+for (let t = 0; t < T; t++) {
+    const N = parseInt(input[idx++]);
+    const data = input[idx++].split(' ').map(Number);
+
+    const teamCounts = {};
+    const teamScores = {};
+    const teamPositions = {}; // 각 팀의 도착 순서 저장
+
+    // 1. 각 팀 선수 수 및 위치 저장
+    data.forEach((team) => {
+        if (!teamCounts[team]) {
+            teamCounts[team] = 0;
+            teamScores[team] = [];
+            teamPositions[team] = [];
+        }
+        teamCounts[team]++;
+    });
+
+    const validTeams = Object.keys(teamCounts)
+        .map(Number)
+        .filter(team => teamCounts[team] >= 6);
+
+    let i = 1
+    data.forEach((team) => {
+        if (validTeams.includes(team)) teamPositions[team].push(i++)
+    })
+
+    // 2. 점수 계산
+    const scoreBoard = [];
+
+    for (const team of validTeams) {
+        const positions = teamPositions[team];
+        const score = positions.slice(0, 4).reduce((a, b) => a + b, 0);
+        const fifth = positions[4]; // 5번째 선수 등수
+        scoreBoard.push({ team, score, fifth });
+    }
+
+    // 3. 정렬
+    scoreBoard.sort((a, b) => {
+        if (a.score !== b.score) return a.score - b.score;
+        return a.fifth - b.fifth;
+    });
+
+    console.log(scoreBoard[0].team);
+}
