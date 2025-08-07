9dc5dcad809ca99a260d96527c2cae4622d87d57
Yongmin Park <75519420+oio337a@users.noreply.github.com>
2025-08-08 00:40:42 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 180 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = parseInt(input[0]);
+const M = parseInt(input[1]);
+const positions = input[2].split(' ').map(Number).sort((a, b) => a - b);
+
+let left = 0;
+let right = N;
+let answer = N;
+
+function isPossible(X) {
+    let prev = 0; // 시작점
+    for (let i = 0; i < M; i++) {
+        const start = positions[i] - X;
+        const end = positions[i] + X;
+
+        if (start > prev) return false; // 중간에 어두운 부분 있음
+        prev = end;
+    }
+    return prev >= N;
+}
+
+while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (isPossible(mid)) {
+        answer = mid;
+        right = mid - 1;
+    } else {
+        left = mid + 1;
+    }
+}
+
+console.log(answer);
