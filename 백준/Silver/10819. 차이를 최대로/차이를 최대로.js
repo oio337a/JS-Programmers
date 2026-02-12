00f476afa61ada90f504422da32ef9004ebcf1a4
Yongmin Park <75519420+oio337a@users.noreply.github.com>
2026-02-12 16:55:12 +0900

    [Silver II] Title: 차이를 최대로, Time: 164 ms, Memory: 10216 KB -BaekjoonHub

---
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const N = Number(input[0])
+const arr = input[1].split(' ').map(Number)
+
+let answer = 0
+const visited = Array(N).fill(false)
+const perm = Array(N)
+
+function dfs(depth) {
+    if (depth === N) {
+        let temp = 0
+        for (let i = 0; i < N - 1; i++) {
+            temp += Math.abs(perm[i] - perm[i + 1])
+        }
+        if (temp > answer) answer = temp
+        return
+    }
+    for (let i = 0; i < N; i++) {
+        if (visited[i]) continue
+        visited[i] = true
+        perm[depth] = arr[i]
+        dfs(depth + 1)
+        visited[i] = false
+    }
+}
+
+dfs(0)
+console.log(answer)
