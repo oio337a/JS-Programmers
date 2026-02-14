e285ae46d5c2f995bf6a433b5a04b32bd94653f9
Yongmin Park <75519420+oio337a@users.noreply.github.com>
2026-02-14 17:32:44 +0900

    [Silver II] Title: 부분수열의 합, Time: 216 ms, Memory: 9616 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [N, S] = input[0].split(' ').map(Number);
+const list = input[1].split(' ').map(Number);
+
+let answer = 0;
+
+// 1부터 (1<<N)-1 까지 → 공집합 제외
+for (let mask = 1; mask < (1 << N); mask++) {
+    let sum = 0;
+    for (let i = 0; i < N; i++) {
+        if (mask & (1 << i)) {  // i번째 원소 포함 여부
+            sum += list[i];
+        }
+    }
+    if (sum === S) answer++;
+}
+
+console.log(answer);
