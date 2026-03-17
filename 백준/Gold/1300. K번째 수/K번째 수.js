f36f49f761ede8406327d790522324f963bb2ddb
Yongmin Park <75519420+oio337a@users.noreply.github.com>
2026-03-17 16:46:43 +0900

    [Gold I] Title: K번째 수, Time: 172 ms, Memory: 11428 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = Number(input[0])
+const K = Number(input[1])
+
+let [s, e] = [1, N * N]
+
+while (s <= e) {
+    const mid = Math.floor((s + e) / 2)
+
+    let cnt = 0
+    for (let i = 1; i < N + 1; i++) cnt += Math.min(Math.floor(mid / i), N)
+
+    if (cnt >= K) e = mid - 1
+    else s = mid + 1
+}
+
+console.log(s)
