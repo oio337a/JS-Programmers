425aac09253b9799539b4d2f51e7622c6931d49c
Yongmin Park <75519420+oio337a@users.noreply.github.com>
2026-03-24 23:36:47 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 780 ms, Memory: 102368 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -1,30 +1,31 @@
-const input = require('fs').readFileSync(0).toString().trim().split('\n');
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const [n, m] = input[0].split(' ').map((e) => +e);
+const [N, M] = input[0].split(" ").map(Number);
+const graph = Array.from({ length: N + 1 }, () => []);
+const visited = Array(N + 1).fill(false);
 
-let node = Array.from(Array(n + 1), () => []);
-let visited = new Array(n + 1).fill(false);
-let answer = 0;
-
-for (let i = 0; i < m; i++) {
-  [a, b] = input[i + 1].split(' ').map((e) => +e);
-  node[a].push(b);
-  node[b].push(a);
+// 간선 입력
+for (let i = 1; i <= M; i++) {
+  const [u, v] = input[i].split(" ").map(Number);
+  graph[u].push(v);
+  graph[v].push(u);
 }
 
-function dfs(visited, v) {
-  visited[v] = true;
-  for (i of node[v]) {
-    if (!visited[i]) {
-      dfs(visited, i);
-    }
+// DFS 함수
+function dfs(node) {
+  visited[node] = true;
+  for (const next of graph[node]) {
+    if (!visited[next]) dfs(next);
   }
 }
 
-for (let i = 1; i <= n; i++) {
+let count = 0;
+for (let i = 1; i <= N; i++) {
   if (!visited[i]) {
-    dfs(visited, i);
-    answer++;
+    dfs(i);
+    count++;
   }
 }
-console.log(answer);
+
+console.log(count);
