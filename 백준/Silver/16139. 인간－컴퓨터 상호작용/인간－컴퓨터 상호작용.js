b492ef340cb583d1ce82c8aacb732f63edfe247d
Yongmin Park <75519420+oio337a@users.noreply.github.com>
2026-04-05 21:57:03 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 244 ms, Memory: 13288 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const [arr, m, ...input] = fs.readFileSync(0).toString().trim().split('\n');
+
+let arr_len = arr.length;
+const prefix = Array.from({ length: 26 }, () => Array(arr_len + 1).fill(0));
+
+// prefix[i][j] = arr[0..j-1]까지에서 알파벳 i의 등장 횟수
+for (let i = 0; i < arr_len; i++) {
+  let code = arr.charCodeAt(i) - 97;
+  for (let k = 0; k < 26; k++) {
+    prefix[k][i + 1] = prefix[k][i];
+  }
+  prefix[code][i + 1]++;
+}
+
+for (let test of input) {
+  let [character, s, e] = test.split(' ');
+  s = Number(s);
+  e = Number(e);
+  let idx = character.charCodeAt(0) - 97;
+  // [s, e] 구간 포함 → prefix[idx][e+1] - prefix[idx][s]
+  console.log(prefix[idx][e + 1] - prefix[idx][s]);
+}
